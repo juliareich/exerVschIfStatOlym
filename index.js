de0439d26f0c9f56ac80532d1e21e4cb2d46dfbe
juliareich <jareich23@gmail.com>
2020-11-13 11:54:25 -0700

    updated assignment

--- index.js
+++ index.js
@@ -64,23 +64,35 @@ if(1 <= 2 && 2 === 4){
 }
 
 //gold medal
+//number 1
 //var word = "dog" had assigned a variable but realized didn't need to
 if(typeof "dog" === "string"){
     console.log("dog")
 }
 
+//number 2
 if(typeof "true" === "boolean"){
     console.log("is boolean")
 } else {
     console.log("is not boolean")
 }
 
+//number 3
 if(typeof x !== "undefined"){
     console.log(x)
 } else {
     console.log("Variable undefined")
 }
 
+//number 4
+if("s" > 12){
+    console.log("s is greater")
+} else {
+    console.log("That doesn't make sense")
+}
+
+
+//number 5 or 6?
 var myNumber = 8
 if(myNumber % 2 === 0){
     console.log("My Number is even!")
